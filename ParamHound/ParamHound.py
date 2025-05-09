4a776012a35429fab3a3cf8dadcf0f179e5be105
Mohamed Gamil <110498922+x0gamil@users.noreply.github.com>
2025-05-09 20:56:52 +0300

    Add files via upload

--- ParamHound/ParamHound.py
+++ ParamHound/ParamHound.py
@@ -5,8 +5,9 @@ GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
+test_values = ["1", "test", "True", "False", "null", "12345", "' OR 1=1 --", "<script>alert(1)</script>"]    
 
-parser = argparse.ArgumentParser(description="ParamHound.py - Fuzzing Hidden Parameters")
+parser = argparse.ArgumentParser(description="hidden.py - Fuzzing Hidden Parameters")
 
 parser.add_argument("-u", "--url" , help='Target Base URL', required=True)
 #parser.add_argument('-p', "--param", help="Parameter To Test", required=True)
@@ -28,13 +29,15 @@ print("\n")
 with open(f"{path_to_wordlist}", "r") as file:
     for param in file:
         param=param.strip()
-        payload = {param: 1}
-        full_url= f"{args.url}?{param}=1"
+        #payload = {param: 1}
+        for value in test_values:
+            payload = {param: value.strip()}
+            full_url= f"{args.url}?{param}={value}"
 
-        response = requests.get(f"{args.url}",params=payload)
+            response = requests.get(f"{args.url}",params=payload)
 
-        print(f"Working on {full_url}")
-        if (response.text != base_response.text) and (len(response.text) != len(base_response.text)):
-            print(f"{GREEN}[+] Potential Existed Parameter: '{param}' >> status code: '{response.status_code}{RESET}'\n")
-        else:
-            print(f"{RED}Parameter not existed: '{param}' >> status code: '{response.status_code}{RESET}'\n")
+            print(f"Working on {full_url}")
+            if (response.text != base_response.text) and (len(response.text) != len(base_response.text)):
+                print(f"{GREEN}[+] Potential Existed Parameter: '{param}' >> status code: '{response.status_code}{RESET}'\n")
+            else:
+                print(f"{RED}Parameter not existed: '{param}' >> status code: '{response.status_code}{RESET}'\n")
